3d3beb5a6bd74af7b11cacabf622a7412441cf15
renebosch <93144011+renebosch@users.noreply.github.com>
2021-11-20 15:51:23 +0100

    Update rauschberg.py

--- rauschberg.py
+++ rauschberg.py
@@ -32,3 +32,29 @@ if r.status_code == 200:
    with open("images/webcam1.jpg", 'wb') as f: 
       r.raw.decode_content = True
       shutil.copyfileobj(r.raw, f)
+      
+      
+# Take the file and upload it to Dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+ 
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+ 
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+ 
+def main():
+    access_token = 'H4YI9GLvXcYAAAAAAAAAAdNkasNTmlKLKqzSI997hxrDCx5OpzUzFIG3CJiHhJv4'
+    transferData = TransferData(access_token)
+ 
+    file_from = 'images/webcam1.jpg' # This is name of the file to be uploaded
+    file_to = '/rauschberg/webcam1.jpg'  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+ 
+    # API v2
+    transferData.upload_file(file_from, file_to)
+ 
+if __name__ == '__main__':
+    main()
